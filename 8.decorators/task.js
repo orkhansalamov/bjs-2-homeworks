755dd74a93a64705a148f1a89cbe4adec44794c6
Orkhan Salamov <developerorkhan@gmail.com>
2022-01-18 14:03:56 +0700

    update

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,54 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cashe = {};
+  let count = 0;
+
+  function wrapper(...args) {
+    const hash = args.join(",");
+    if (hash in cashe) {
+      return "Из кэша: " + cashe[hash];
+    }
+    if (count >= 5) {
+      delete cashe[Object.keys(cashe)[0]];
+    }
+    count++
+    cashe[hash] = func(...args);
+    return "Вычисляем: " + cashe[hash];
+  }
+  return wrapper;
 }
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
-}
+  let flag = false;
+  
+  return function(...args) {
+    if (flag) {
+      return;
+    }
+      func.apply(this, args)
+      flag = true;
+      setTimeout(() => {
+        flag = false;
+        func.apply(this, args)
+      }, timeout);
+    }
+  }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let flag = false;
+
+  function wrapper(...args) {
+    if (flag) {
+      return;
+    }
+    wrapper.count++;
+    func.apply(this,args);
+    flag = true;
+    setTimeout(() => {
+      flag = false;
+      func.apply(this, args);
+    }, timeout);
+  }
+  wrapper.count = 0;
+  return wrapper;
 }
