14fb1603fd3df4a553611e624a0d377fdb2d591d
Orkhan Salamov <developerorkhan@gmail.com>
2022-02-17 15:40:18 +0700

    update

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,54 +1,88 @@
-function cachingDecoratorNew(func) {
-  let cashe = {};
-  let count = 0;
 
-  function wrapper(...args) {
+// Задача 1. Усовершенствуйте кэширующий декоратор
+function cachingDecoratorNew(func){
+  let cache = [];
+
+  function wrapper(...args){
     const hash = args.join(",");
-    if (hash in cashe) {
-      return "Из кэша: " + cashe[hash];
+    let index = cache.findIndex((item) => cache.item  === hash);
+    if(index !== -1){
+      console.log("Из кэша: " + index);
+      return "Из кэша: " + index;
     }
-    if (count >= 5) {
-      delete cashe[Object.keys(cashe)[0]];
+
+    let result = func(...args);
+    cache.push(result);
+    if(cache.length > 5){
+      cache.shift();
     }
-    count++
-    cashe[hash] = func(...args);
-    return "Вычисляем: " + cashe[hash];
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
   }
+
   return wrapper;
 }
 
+// Задача 2. Debounce декоратор с моментальным вызовом
 
-function debounceDecoratorNew(func) {
+
+function debounceDecoratorNew(func, time){
   let flag = false;
+  let timeOut;
+
+  function wrapper(...args){
   
-  return function(...args) {
-    if (flag) {
-      return;
-    }
-      func.apply(this, args)
+    if(flag === false){
+      func(...args)
       flag = true;
-      setTimeout(() => {
+
+      timeOut = setTimeout(()=>{
+        func(...args);
         flag = false;
-        func.apply(this, args)
-      }, timeout);
+      },time);
+    }else{
+      timeOut = args;
     }
   }
-
-function debounceDecorator2(func) {
-  let flag = false;
-
-  function wrapper(...args) {
-    if (flag) {
-      return;
-    }
-    wrapper.count++;
-    func.apply(this,args);
-    flag = true;
-    setTimeout(() => {
-      flag = false;
-      func.apply(this, args);
-    }, timeout);
-  }
-  wrapper.count = 0;
   return wrapper;
 }
+
+
+// Задача 3. Усовершенствуйте debounceDecoratorNew
+function debounceDecorator2(func, time){
+   let flag = false;
+   let timeOut;
+   let count;
+   let sendSignalCount = [];
+
+   function wrapper(...args){
+     if(flag === false){
+       func(...args)
+        flag = true;
+          count++;
+            sendSignalCount.push(count);
+              console.log(`Сигнал был послан ${sendSignalCount.length} раз(а)`);
+       timeOut = setTimeout(() =>{
+        func(...args)
+          count++;
+            sendSignalCount.push(count);
+              console.log(`Сигнал был послан ${sendSignalCount.length} раз(а)`);
+         flag = false;
+       }, time);
+     }else{
+       timeOut = args;
+     }
+   }
+   return wrapper;
+}
+
+
+const sendSignal = () => console.log("Сигнал отправлен" );
+const upgradedSendSignal = debounceDecorator2(sendSignal, 2000);
+setTimeout(upgradedSendSignal); // Сигнал отправлен
+setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
+setTimeout(upgradedSendSignal, 900); // проигнорировано так как времени от последнего вызова прошло: 900-300=600 (600 < 2000)
+setTimeout(upgradedSendSignal, 1200); // проигнорировано так как времени от последнего вызова прошло: 1200-900=300 (300 < 2000)
+setTimeout(upgradedSendSignal, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
+setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
+setTimeout(upgradedSendSignal, 4500); // С
